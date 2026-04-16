0fc720e7d7204cbe8f14d6400b36ef0154943240
leomf-dev <leonelmelendezfalcon@gmail.com>
2026-04-16 17:58:49 -0500

    Initial Commit

---
+++ HealthyGO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthyGO
+//
+//  Created by DESIGN on 16/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
